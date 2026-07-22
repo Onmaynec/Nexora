64e5763e90b4f6e174c61067efada1ae92257da2
Kufik <127012125+Onmaynec@users.noreply.github.com>
2026-07-23 02:32:23 +0500

    feat(pulse): register catalog billing methods

--- cloud/billing-core.cjs
+++ cloud/billing-core.cjs
@@ -14,9 +14,10 @@ const {
 const accountLedgerMethods = require("./billing-account-ledger.cjs");
 const subscriptionMethods = require("./billing-subscriptions.cjs");
 const goalMethods = require("./billing-goals.cjs");
+const catalogMethods = require("./billing-catalog.cjs");
 
 class BillingDatabase extends BillingBase {}
-Object.assign(BillingDatabase.prototype, accountLedgerMethods, subscriptionMethods, goalMethods);
+Object.assign(BillingDatabase.prototype, accountLedgerMethods, subscriptionMethods, goalMethods, catalogMethods);
 
 module.exports = {
   BillingDatabase,
